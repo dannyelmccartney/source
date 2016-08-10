9ecbe1bfb20251d56419f6fcd43b1f124c13679a
Luiz Eduardo <luiz.eduardo@mrpbrasil.com>
2016-08-10 10:51:42 -0300

    Decrease thor dificulty

--- basicBot.js
+++ basicBot.js
@@ -3307,8 +3307,8 @@
                               indexArrUsedThor,
                               thorCd = false,
                               timeInMinutes = 0,
-                              worthyAlg = Math.floor(Math.random() * 10),
-                              worthy = worthyAlg == 10 ? true : false;
+                              worthyAlg = Math.floor(Math.random() * 7),
+                              worthy = worthyAlg == 0 ? true : false;
 
                           for (var i = 0; i < djlist.length; i++) {
                               if (djlist[i].id == id)
